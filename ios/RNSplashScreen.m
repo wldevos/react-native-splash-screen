e6b1d392c09fb48f1041a83131b0f151cf205215
wldevos <38913230+wldevos@users.noreply.github.com>
2019-08-27 10:34:29 +0200

    Update RNSplashScreen.m

--- ios/RNSplashScreen.m
+++ ios/RNSplashScreen.m
@@ -10,9 +10,11 @@
 #import "RNSplashScreen.h"
 #import <React/RCTBridge.h>
 
-static bool waiting = true;
+static bool waiting = false;
 static bool addedJsLoadErrorObserver = false;
 static UIView* loadingView = nil;
+static UIView* srootView = nil;
+static NSString* ssplashScreen = nil;
 
 @implementation RNSplashScreen
 - (dispatch_queue_t)methodQueue{
@@ -25,40 +27,53 @@ RCT_EXPORT_MODULE(SplashScreen)
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(jsLoadError:) name:RCTJavaScriptDidFailToLoadNotification object:nil];
         addedJsLoadErrorObserver = true;
     }
-
-    while (waiting) {
-        NSDate* later = [NSDate dateWithTimeIntervalSinceNow:0.1];
-        [[NSRunLoop mainRunLoop] runUntilDate:later];
+    
+    // while (waiting) {
+    // NSDate* later = [NSDate dateWithTimeIntervalSinceNow:0.1];
+    // [[NSRunLoop mainRunLoop] runUntilDate:later];
+    // }
+    
+    if (!loadingView) {
+        loadingView = [[[NSBundle mainBundle] loadNibNamed:ssplashScreen owner:self options:nil] objectAtIndex:0];
+        CGRect frame = srootView.frame;
+        frame.origin = CGPointMake(0, 0);
+        loadingView.frame = frame;
     }
+    // waiting = false;
+    
+    [srootView addSubview:loadingView];
 }
 
 + (void)showSplash:(NSString*)splashScreen inRootView:(UIView*)rootView {
     if (!loadingView) {
         loadingView = [[[NSBundle mainBundle] loadNibNamed:splashScreen owner:self options:nil] objectAtIndex:0];
+        srootView = rootView;
+        ssplashScreen = splashScreen;
         CGRect frame = rootView.frame;
         frame.origin = CGPointMake(0, 0);
         loadingView.frame = frame;
     }
-    waiting = false;
+    // waiting = false;
     
     [rootView addSubview:loadingView];
 }
 
 + (void)hide {
-    if (waiting) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            waiting = false;
-        });
-    } else {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [loadingView removeFromSuperview];
-        });
-    }
+    // if (waiting) {
+    // dispatch_async(dispatch_get_main_queue(),
+    // ^{
+    // waiting = false;
+    // });
+    // }
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [loadingView removeFromSuperview];
+    });
 }
 
 + (void) jsLoadError:(NSNotification*)notification
 {
-    // If there was an error loading javascript, hide the splash screen so it can be shown.  Otherwise the splash screen will remain forever, which is a hassle to debug.
+    // If there was an error loading javascript, hide the splash screen so it can be shown. Otherwise the splash screen will remain forever, which is a hassle to debug.
     [RNSplashScreen hide];
 }
 
